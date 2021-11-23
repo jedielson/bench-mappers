8bda5932eefbf2680c17702484b73ff50f2cee2f
Jedi <12877446+jedielson@users.noreply.github.com>
2021-11-23 12:31:49 -0300

    Creating solution and projects

---
+++ benchmark/BenchMappers.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BenchMappers.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
